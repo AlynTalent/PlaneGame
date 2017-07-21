f66b6f237f35e3f3c36f7bba7b788d12a28d93b1
sam12 <sam12d@my.fsu.edu>
2017-07-21 19:52:40 -0400

    added Cloud img and Background color

--- app/src/main/java/edu/fsu/cs/mobile/planegame/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/planegame/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -12,17 +14,20 @@ import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //Member variables
     private float xPos, xAccel, xVel = 0.0f;
-    private float yPos = 800;
+    private float yPos;
     private float xMax;
-    private Bitmap plane;
+    private Bitmap plane, cloud;
     private SensorManager sensorManager;
     SensorEventListener2 seListener;
+
+    int height, width;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +37,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(planeView);
 
         Point size = new Point();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
         Display display = getWindowManager().getDefaultDisplay();
         display.getSize(size);
+
         xMax = (float) size.x - 200;
+        yPos = size.y - 450;
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -89,14 +97,26 @@ public class MainActivity extends AppCompatActivity {
         public PlaneView(Context context){
             super(context);
             Bitmap planeSrc = BitmapFactory.decodeResource(getResources(), R.drawable.plane);
+            Bitmap cloudSrc = BitmapFactory.decodeResource(getResources(), R.drawable.cloud);
             final int dstWidth = 200;
             final int dstHeight = 200;
             plane = Bitmap.createScaledBitmap(planeSrc, dstWidth, dstHeight, true);
+            cloud = Bitmap.createScaledBitmap(cloudSrc, 300, 200, true);
         }
 
         @Override
         protected void onDraw(Canvas canvas){
+            Paint paint = new Paint();
+            paint.setTextSize(50);
+            paint.setColor(Color.BLACK);
+
+            int Sky = Color.rgb(135, 206, 250);
+            canvas.drawColor(Sky);
+            canvas.drawText("Score: ", 700, 200, paint);
+            canvas.drawBitmap(cloud, 100, 100, null);
+            canvas.drawBitmap(cloud, 500, 500, null);
             canvas.drawBitmap(plane, xPos, yPos, null);
+
             invalidate();
         }
     }
